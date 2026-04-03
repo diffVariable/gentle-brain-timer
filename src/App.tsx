c1240658981fa8ed4ba7b60341c32a6e9235e504
diffVariable <engg.diana@gmail.com>
2026-04-04 03:24:45 +0800

    draft setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import TimerPage from "./pages/TimerPage";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background:
+          "linear-gradient(135deg, #fde8f0 0%, #ede0f8 50%, #fde8f0 100%)",
+      }}
+    >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/timer" element={<TimerPage />} />
+      </Routes>
+    </div>
+  );
+}
